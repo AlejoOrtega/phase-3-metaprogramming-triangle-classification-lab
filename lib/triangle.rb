9b25f26de09a6f83cec35625b01bee5ea51f8460
Alejandro Ortega <43015395+AlejoOrtega@users.noreply.github.com>
2022-05-19 09:48:23 -0400

    commit tree dd64be819b164f4002bd01c09bce0ba87de1821c

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
 class Triangle
   # write code here
+  class TriangleError < StandardError
+    def initialize(msg="My default message")
+      super
+    end
+  end
+
+
+
+  attr_reader :side1, :side2, :side3
+  def initialize( side1, side2, side3)
+    side1, side2, side3 = [side1, side2, side3].sort
+    if (side1 + side2) <= side3
+      raise TriangleError.new('The sum of two sides are not greater than the third ')
+    elsif side1 <=0 || side2 <= 0 || side3 <= 0
+      raise TriangleError.new('Sides cant be 0 or negative')
+    end
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    if [@side1, @side2, @side3].uniq.length == 1
+      :equilateral
+    elsif [@side1, @side2, @side3].uniq.length == 2
+      :isosceles
+    elsif [@side1, @side2, @side3].uniq.length == 3
+      :scalene
+    else
+
+    end
+  end
 end
